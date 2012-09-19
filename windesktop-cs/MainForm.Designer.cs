1e7fceb401efe802a96847cfacdf122b6ba69067
Nat Morris <nat@nuqe.net>
2012-09-19 02:07:28 +0100

    Original Windows desktop app, needs rewriting to run on OS X

---
+++ windesktop-cs/MainForm.Designer.cs
@@ -0,0 +1,210 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nat
+ * Date: 04/11/2011
+ * Time: 15:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace scansort
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.treeFolders = new System.Windows.Forms.TreeView();
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.gridFiles = new System.Windows.Forms.DataGridView();
+			this.Filename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.picPreview = new System.Windows.Forms.PictureBox();
+			this.dateFile = new System.Windows.Forms.DateTimePicker();
+			this.txtFilename = new System.Windows.Forms.TextBox();
+			this.txtSuffix = new System.Windows.Forms.TextBox();
+			this.btnMove = new System.Windows.Forms.Button();
+			this.lstPreview = new System.Windows.Forms.ListBox();
+			this.lblFiles = new System.Windows.Forms.Label();
+			this.lblOverYear = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.gridFiles)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picPreview)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// treeFolders
+			// 
+			this.treeFolders.ImageKey = "folder";
+			this.treeFolders.ImageList = this.imageList1;
+			this.treeFolders.Location = new System.Drawing.Point(12, 296);
+			this.treeFolders.Name = "treeFolders";
+			this.treeFolders.SelectedImageIndex = 0;
+			this.treeFolders.Size = new System.Drawing.Size(342, 308);
+			this.treeFolders.TabIndex = 0;
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "folder");
+			// 
+			// gridFiles
+			// 
+			this.gridFiles.AllowUserToAddRows = false;
+			this.gridFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.gridFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+									this.Filename,
+									this.Date});
+			this.gridFiles.Location = new System.Drawing.Point(12, 29);
+			this.gridFiles.Name = "gridFiles";
+			this.gridFiles.ReadOnly = true;
+			this.gridFiles.RowHeadersVisible = false;
+			this.gridFiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.gridFiles.Size = new System.Drawing.Size(342, 261);
+			this.gridFiles.TabIndex = 1;
+			this.gridFiles.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridFilesCellContentClick);
+			this.gridFiles.SelectionChanged += new System.EventHandler(this.GridFilesSelectionChanged);
+			// 
+			// Filename
+			// 
+			this.Filename.HeaderText = "Filename";
+			this.Filename.Name = "Filename";
+			this.Filename.ReadOnly = true;
+			// 
+			// Date
+			// 
+			this.Date.HeaderText = "Date";
+			this.Date.Name = "Date";
+			this.Date.ReadOnly = true;
+			// 
+			// picPreview
+			// 
+			this.picPreview.Location = new System.Drawing.Point(374, 12);
+			this.picPreview.Name = "picPreview";
+			this.picPreview.Size = new System.Drawing.Size(699, 842);
+			this.picPreview.TabIndex = 2;
+			this.picPreview.TabStop = false;
+			this.picPreview.Click += new System.EventHandler(this.PictureBox1Click);
+			// 
+			// dateFile
+			// 
+			this.dateFile.Location = new System.Drawing.Point(12, 610);
+			this.dateFile.Name = "dateFile";
+			this.dateFile.Size = new System.Drawing.Size(200, 20);
+			this.dateFile.TabIndex = 4;
+			this.dateFile.ValueChanged += new System.EventHandler(this.DateTimePicker1ValueChanged);
+			// 
+			// txtFilename
+			// 
+			this.txtFilename.Location = new System.Drawing.Point(12, 695);
+			this.txtFilename.Name = "txtFilename";
+			this.txtFilename.Size = new System.Drawing.Size(217, 20);
+			this.txtFilename.TabIndex = 5;
+			this.txtFilename.TextChanged += new System.EventHandler(this.TxtFilenameTextChanged);
+			// 
+			// txtSuffix
+			// 
+			this.txtSuffix.Location = new System.Drawing.Point(12, 650);
+			this.txtSuffix.Name = "txtSuffix";
+			this.txtSuffix.Size = new System.Drawing.Size(189, 20);
+			this.txtSuffix.TabIndex = 6;
+			this.txtSuffix.TextChanged += new System.EventHandler(this.TxtSuffixTextChanged);
+			// 
+			// btnMove
+			// 
+			this.btnMove.Location = new System.Drawing.Point(252, 692);
+			this.btnMove.Name = "btnMove";
+			this.btnMove.Size = new System.Drawing.Size(75, 23);
+			this.btnMove.TabIndex = 7;
+			this.btnMove.Text = "Move";
+			this.btnMove.UseVisualStyleBackColor = true;
+			this.btnMove.Click += new System.EventHandler(this.BtnMoveClick);
+			// 
+			// lstPreview
+			// 
+			this.lstPreview.FormattingEnabled = true;
+			this.lstPreview.Location = new System.Drawing.Point(12, 736);
+			this.lstPreview.Name = "lstPreview";
+			this.lstPreview.Size = new System.Drawing.Size(342, 108);
+			this.lstPreview.TabIndex = 8;
+			// 
+			// lblFiles
+			// 
+			this.lblFiles.Location = new System.Drawing.Point(12, 12);
+			this.lblFiles.Name = "lblFiles";
+			this.lblFiles.Size = new System.Drawing.Size(342, 14);
+			this.lblFiles.TabIndex = 9;
+			this.lblFiles.Text = "label1";
+			// 
+			// lblOverYear
+			// 
+			this.lblOverYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblOverYear.ForeColor = System.Drawing.Color.Red;
+			this.lblOverYear.Location = new System.Drawing.Point(218, 610);
+			this.lblOverYear.Name = "lblOverYear";
+			this.lblOverYear.Size = new System.Drawing.Size(100, 23);
+			this.lblOverYear.TabIndex = 10;
+			this.lblOverYear.Text = "> 1yr";
+			this.lblOverYear.Visible = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1085, 866);
+			this.Controls.Add(this.lblOverYear);
+			this.Controls.Add(this.lblFiles);
+			this.Controls.Add(this.lstPreview);
+			this.Controls.Add(this.btnMove);
+			this.Controls.Add(this.txtSuffix);
+			this.Controls.Add(this.txtFilename);
+			this.Controls.Add(this.dateFile);
+			this.Controls.Add(this.picPreview);
+			this.Controls.Add(this.gridFiles);
+			this.Controls.Add(this.treeFolders);
+			this.Name = "MainForm";
+			this.Text = "scansort";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.gridFiles)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picPreview)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label lblOverYear;
+		private System.Windows.Forms.Label lblFiles;
+		private System.Windows.Forms.ListBox lstPreview;
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Button btnMove;
+		private System.Windows.Forms.TextBox txtSuffix;
+		private System.Windows.Forms.TextBox txtFilename;
+		private System.Windows.Forms.DateTimePicker dateFile;
+		private System.Windows.Forms.PictureBox picPreview;
+		private System.Windows.Forms.DataGridView gridFiles;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Filename;
+		private System.Windows.Forms.TreeView treeFolders;
+	}
+}
---
+++ windesktop-cs/MainForm.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace scansort
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private DirectoryInfo mDest;
+		private DirectoryInfo mSrc;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			mDest = new DirectoryInfo(@"C:\Users\nat\Dropbox\Household");
+			mSrc = new DirectoryInfo(@"C:\Users\nat\Dropbox\Household\unsorted june 2011");
+			PopulateFilelist();
+			PopulateDestinationTree();
+		}
+		
+		private void PopulateDestinationTree(	)
+		{
+			treeFolders.Nodes.Clear();
+			
+			TreeNode root = new TreeNode(mDest.Name);
+			
+			root.Tag = mDest.FullName;
+			
+			treeFolders.Nodes.Add(root);
+			
+			foreach (DirectoryInfo d in mDest.GetDirectories())
+			{
+				TreeNode n = new TreeNode(d.Name);
+				n.Tag = d.FullName;
+				root.Nodes.Add(n);
+			}
+			root.ExpandAll();
+		}
+		
+		private void PopulateFilelist()
+		{
+			gridFiles.Rows.Clear();
+			int files = 0 ;
+			foreach (FileInfo f in mSrc.GetFiles())
+			{
+				if (f.Name.ToLower().EndsWith(".jpg")) {
+					DataGridViewRow r = new DataGridViewRow();
+					r.Tag = f.FullName;
+					
+					r.Cells.Add(NewTextCell(f.Name));
+					r.Cells.Add(NewTextCell(f.CreationTime.ToShortDateString()));
+					gridFiles.Rows.Add(r);
+				}
+				files++;
+			}
+			lblFiles.Text = String.Format("{0} unsorted files",files);
+		}
+		
+		private DataGridViewTextBoxCell NewTextCell(string text)
+		{
+			DataGridViewTextBoxCell c = new DataGridViewTextBoxCell();
+			c.Value = text;
+			return c;
+		}
+		
+		void PictureBox1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void GridFilesCellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			
+		}
+		
+		void GridFilesSelectionChanged(object sender, EventArgs e)
+		{
+			if (gridFiles.SelectedRows.Count > 0)
+			{
+				
+				string tag = gridFiles.SelectedRows[0].Tag.ToString();
+				Image imgToResize = new Bitmap(tag);
+				
+				
+				
+				Size size = new Size(picPreview.Width,1024);
+				
+				
+				
+				
+				
+				int sourceWidth = imgToResize.Width;
+  				 int sourceHeight = imgToResize.Height;
+				
+  				float nPercent = 0;
+			   float nPercentW = 0;
+			   float nPercentH = 0;
+			 nPercentW = ((float)size.Width / (float)sourceWidth);
+			   nPercentH = ((float)size.Height / (float)sourceHeight);
+			
+			   if (nPercentH < nPercentW)
+			      nPercent = nPercentH;
+			   else
+			      nPercent = nPercentW;
+   
+				int destWidth = (int)(sourceWidth * nPercent);
+			   int destHeight = (int)(sourceHeight * nPercent);
+			
+			   Bitmap b = new Bitmap(destWidth, destHeight);
+			   Graphics g = Graphics.FromImage((Image)b);
+			   //g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+			
+			   g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+			   g.Dispose();
+				picPreview.Image = (Image)b;;
+				ResetFilename();
+				imgToResize.Dispose();
+			}
+			UpdateFilename();
+		}
+		
+		private void RefreshSourceFile()
+		{
+			
+		}
+		
+		private void ResetFilename()
+		{
+			txtSuffix.Text = "";
+		}
+		
+		void DateTimePicker1ValueChanged(object sender, EventArgs e)
+		{
+			UpdateFilename();
+			TimeSpan ts = DateTime.Now - dateFile.Value;
+			if (ts.TotalDays > 365)
+			{
+				lblOverYear.Visible = true;
+			} else {
+				lblOverYear.Visible = false;
+			}
+		}
+		
+		private void UpdateFilename()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append(dateFile.Value.ToString("yyyy-MM-dd"));
+			if (txtSuffix.Text.Length > 0)
+			{
+				sb.Append(" ");
+				sb.Append(txtSuffix.Text);
+			}
+			txtFilename.Text = sb.ToString();
+			
+			List<string> dstfiles = GetDestFilenames(txtFilename.Text);
+			lstPreview.Items.Clear();
+			foreach (string d in dstfiles)
+			{
+				lstPreview.Items.Add(d);
+			}
+		}
+		
+		void TxtSuffixTextChanged(object sender, EventArgs e)
+		{
+			UpdateFilename();
+		}
+		
+		void BtnMoveClick(object sender, EventArgs e)
+		{
+			if (treeFolders.SelectedNode != null)
+			{
+				string dstfolder = treeFolders.SelectedNode.Tag.ToString();
+				bool success = MoveFiles(dstfolder,txtFilename.Text);
+				
+				if (success)
+				{
+					PopulateFilelist();
+					ResetFilename();
+					txtFilename.Text = "";
+				}
+			}
+		}
+		
+		
+		
+		private bool MoveFiles( string dstfolder, string dstpattern)
+		{
+			List<string> dstfiles = GetDestFilenames(dstpattern);
+			bool allok = true;
+			foreach (string d in dstfiles)
+			{
+				string full = String.Format("{0}\\{1}",dstfolder,d);
+				if (FileExists(full))
+				{
+					allok = false;
+				}
+			}
+			
+			if (allok)
+			{
+				List<string> src = GetSrcFiles();	
+				for (int i = 0; i < src.Count; i++) {
+					string srcfile = src[i];
+					
+					string dstfile = String.Format("{0}\\{1}",dstfolder, dstfiles[i]);
+					FileInfo fi = new FileInfo(srcfile);
+					fi.MoveTo(dstfile);
+				}
+				return true;	
+			} else {
+				MessageBox.Show("File already exists");
+				return false;
+			}
+		}
+		
+		private List<string> GetSrcFiles()
+		{
+			List<string> src = new List<string>();
+			if (gridFiles.SelectedRows.Count > 1)
+			{
+				// multiple files	
+				for (int s = 0; s < gridFiles.SelectedRows.Count; s++) {
+					src.Add(gridFiles.SelectedRows[s].Tag.ToString());
+				}
+			}
+			if (gridFiles.SelectedRows.Count == 1) {
+				// single file
+				src.Add(gridFiles.SelectedRows[0].Tag.ToString());
+			}
+			src.Sort();
+			return src;			
+		}
+		
+		private List<string> GetDestFilenames(string dstfile)
+		{
+			List<string> src = GetSrcFiles();
+			List<string> dstfiles = new List<string>();
+			if (src.Count == 1)
+			{
+				// single
+				dstfiles.Add(dstfile + ".jpg");
+			} else {
+				// multi
+				for (int p = 0; p < src.Count; p++) {
+					string filename = String.Format("{0} pg{1}.jpg",dstfile,(p + 1));
+					dstfiles.Add(filename);
+				}
+			}
+			
+			return dstfiles;
+		}
+		
+		private bool FileExists(string filename)
+		{
+			FileInfo f = new FileInfo(filename);
+			return f.Exists;
+		}
+		
+		void TxtFilenameTextChanged(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
